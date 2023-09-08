3e2185698eb8d3098439236431a3e83d374dd7bc
Emilie RM <romand-monnier@abes.fr>
2023-09-08 16:39:52 +0200

    1er essao

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,8 +18,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ABES', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Abes', // Usually your GitHub org/user name.
+  projectName: 'projet2024', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
