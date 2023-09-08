0317a597f7b62cd48a55c4c8228f51088fb1b6b0
Stéphane Gully <gully@abes.fr>
2023-09-08 16:09:41 +0200

    test autodeploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'ABES - Projet d\'établissement 2024-2028',
+  title: 'Abes - Projet d\'établissement 2024-2028',
   tagline: '',
   favicon: 'img/favicon.ico',
 
@@ -64,7 +64,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Abes',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
