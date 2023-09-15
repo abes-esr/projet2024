1f6ea52f4466c26a08c1fb1974093d2b486122f2
Emilie RM <romand-monnier@abes.fr>
2023-09-15 23:12:30 +0200

    3 versions : en cours / version 1.1 / version 1.0

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,6 +61,10 @@ const config = {
         ({
             // Replace with your project's social card
             //  image: 'img/docusaurus-social-card.jpg',
+            tableOfContents: {
+                minHeadingLevel: 2,
+                maxHeadingLevel: 5,
+            },
             navbar: {
                 title: 'Projet d\'établissement de l\'Abes',
                 logo: {
