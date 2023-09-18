75e8b2096fbd07944b43e1e4720b26aa87ffed1c
Stéphane Gully <gully@abes.fr>
2023-09-18 18:20:01 +0200

    Revert "fix: ajout d'un peu de contenu pour bien vérifier que les commentaires fonctionnent bien"
    This reverts commit 69305cd28e57ee81262f0485c2c9ae07da322af2.

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,33 +5,11 @@ import styles from './styles.module.css';
 const FeatureList = [
 
   {
-    title: 'Pourquoi ce site web ?',
-    Svg: require('@site/static/img/logo-abes-cercle-130x130.svg').default,
+    title: 'Travaux en cours',
+    Svg: require('@site/static/img/Lavori-in-corso.svg').default,
     description: (
       <>
-<p>
-La présentation au Conseil d’Administration de l’Abes, lors de la séance du 14 septembre 2023, des résultats des travaux issus de la réflexion menée par l’agence pour la mise en place de son projet d’établissement 2024-2028 marque la fin d'une étape déterminante du processus. 
-</p>
-
-
-<p>
-En effet, le travail réalisé depuis le début de l’année en interne à l’Abes, avec un panel d’interlocuteurs extérieurs, à l'occasion des Jabes, avec les parties prenantes, appuyé par l’accompagnement d’un consultant, a permis de formaliser une version initiale de notre projet d’établissement communiquée aux personnels de l’Abes puis présentée au conseil d’administration de l'agence. 
-</p>
-
-<p>
-Les contributions et observations recueillies en interne à l’Abes, auprès du Conseil d’Administration, puis le travail réalisé par l’équipe projet ont permis de développer le contenu du projet dans une nouvelle version enrichie que je souhaite aujourd’hui vous soumettre afin de recueillir vos avis.  
-</p>
-
-<p>
-L’objectif de cette consultation est d’en consolider la teneur avec vos retours en vue de la rédaction finale d’un projet précis, complet, au plus près des attentes et besoins de la communauté. Il sera présenté et soumis à validation du conseil d’administration de l’Abes le 20 novembre 2023. 
-</p>
-
-<p>
-Comptant sur votre précieuse implication et collaboration dans cette démarche, je vous remercie pour le temps que vous pourrez consacrer à la lecture du présent document.
-Compte tenu du calendrier des opérations à venir, la consultation du document est accessible jusqu’au 31/12/2023. 
-</p>
-
-
+Le projet d'établissement 2024-2028 de l'Abes sera prochainement publié ici et ouvert aux commentaires.
       </>
     ),
   },
